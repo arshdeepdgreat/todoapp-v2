6883820b6d73389e49d4f76b3f21e251a671381c
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-08 10:54:58 +0530

    Commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js");
 const mongoose = require("mongoose");
+const _ = require("lodash");
+
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistdb");
 
